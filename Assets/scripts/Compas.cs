1bf5a891581df310fc77c083ede0eb1bb121da68
rj-90 <ra0633a@american.edu>
2018-12-02 18:29:09 -0800

    compass
    compass to point north

---
+++ Assets/scripts/Compas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Orient an object to point to magnetic north
+
+        //need to update the z axsis  only so it looks north 
+        transform.rotation = Quaternion.Euler(0, -Input.compass.magneticHeading, 0);
+    }
+
+    }
